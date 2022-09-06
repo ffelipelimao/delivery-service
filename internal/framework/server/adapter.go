24691848c86f9adac5b06c008165901b7719509e
Luiz Felipe Limao <lflimao@hotmail.com>
2022-09-05 23:20:50 -0300

    fix params to private

--- internal/framework/server/adapter.go
+++ internal/framework/server/adapter.go
@@ -11,7 +11,7 @@ func AdaptRoute(controller presentation.Controller) func(c *gin.Context) {
 		queries := c.Request.URL.Query()
 		presentationParam := make(map[string]string)
 
-		for _, param := range Params {
+		for _, param := range params {
 			value := c.Param(param)
 			if value != "" {
 				presentationParam[param] = value
